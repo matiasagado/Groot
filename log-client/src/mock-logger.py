155ce3fefea0a21a9ef4cb4dc18ad9b27a213ef7
Cheonhong (Jun) Lee <clee95@dons.usfca.edu>
2024-10-03 12:04:34 -0700

    changed mock logger so it reads logs and adds it to redis

--- log-client/src/mock-logger.py
+++ log-client/src/mock-logger.py
@@ -2,6 +2,7 @@ import time
 import os
 import redis
 import argparse
+from ingestor import read_log_file
 
 
 # Get Redis host and port from environment variables
@@ -16,11 +17,23 @@ args = parser.parse_args()
 # Connect to Redis
 r = redis.Redis(host=redis_host, port=redis_port)
 
-# Read the file line by line
+# testing if it would read the test.txt file
+# TODO: Improve read_log_file
+# read_log_file("/app/test.txt")
+# r.lpush("log_queue", "test")
+
+# Read the file line by line from 5-line.log inside logs/dmesg
 with open(args.file, "r") as file:
     for line in file:
         # Put each line into the Redis queue
+        print("Adding items: ", line, flush=True)
         r.lpush("log_queue", line.strip())
+
+# This would read the test.txt and add it into the redis queue
+# with open("/app/test.txt", "r") as file:
+#     for line in file:
+#         # Put each line into the Redis queue
+#         r.lpush("log_queue", line.strip())
 
 print("Items added to Redis queue successfully!", flush=True)
 
