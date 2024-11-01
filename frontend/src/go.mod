d3fdf0eedd9390ccccf1757bb9899587e029278b
StealthBadger747 <ac130kire@gmail.com>
2024-10-31 21:02:52 -0700

    Add frontend

---
+++ frontend/src/go.mod
@@ -0,0 +1,29 @@
+module log-parse-frontend
+
+go 1.22.0
+
+require github.com/labstack/echo/v4 v4.12.0
+
+require (
+	github.com/uptrace/go-clickhouse v0.3.1
+	github.com/uptrace/go-clickhouse/chdebug v0.3.1
+)
+
+require (
+	github.com/codemodus/kace v0.5.1 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	go.opentelemetry.io/otel v1.31.0 // indirect
+	go.opentelemetry.io/otel/trace v1.31.0 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+)
---
+++ frontend/src/server.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+// type Log struct {
+// 	Message string
+// }
+
+type Log struct {
+	Dt              time.Time
+	File            string
+	Host            string
+	Level           *string
+	UserDefined     string
+	OriginalMessage string
+	Platform        string
+	UUID            [16]byte
+}
+
+// e.POST("/filterLogs", filter)
+func filterLogs(c echo.Context) error {
+	// Get team and member from the query string
+	filter := c.FormValue("sql-filter")
+	println("SQL filter:", filter)
+
+	// default.vector_logs_experiment_2
+	query := fmt.Sprintf("FROM default.vector_logs_experiment_2 %s;", filter)
+	rows := make_clickhouse_query(query)
+	defer rows.Close()
+	var logRows []Log
+	for rows.Next() {
+		var logResult Log
+		// Ensure the order and types of the arguments match the database columns
+		if err := rows.Scan(
+			&logResult.Dt,
+			&logResult.File,
+			&logResult.Host,
+			&logResult.Level,
+			&logResult.UserDefined,
+			&logResult.OriginalMessage,
+			&logResult.Platform,
+			&logResult.UUID,
+		); err != nil {
+			log.Fatal(err) // Consider handling the error more gracefully
+		}
+		logRows = append(logRows, logResult)
+	}
+
+	println("Hello!")
+	// Render the template, check for errors
+	if err := c.Render(http.StatusOK, "log-row.html", logRows); err != nil {
+		c.Logger().Error(err) // Log the error
+		return c.String(http.StatusInternalServerError, "Error rendering template")
+	}
+
+	return nil
+}
+
+func main() {
+	e := echo.New()
+
+	e.Renderer = &Template{
+		templates: template.Must(template.ParseGlob("content/private/views/*.html")),
+	}
+
+	e.Static("/", "content/public")
+	e.Static("/static", "content/public/static")
+
+	e.POST("/filterLogs", filterLogs)
+
+	e.Logger.Fatal(e.Start("0.0.0.0:1323"))
+}
